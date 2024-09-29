2045f1540ca5041f786fd090f48baa90ee850291
Emile-el38 <st10437928@vcconnect.edu.za>
2024-09-29 18:30:56 +0200

    start
    lets see

---
+++ src/main/java/com/mycompany/speedprac2/Speedprac2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.speedprac2;
+
+/**
+ *
+ * @author denit
+ */
+public class Speedprac2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
